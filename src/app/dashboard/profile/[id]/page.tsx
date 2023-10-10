cc015f4553193dd5e62824527005531009a62944
Arjay Onan <archessonan.1@gmail.com>
2023-10-10 13:56:51 +0800

    modified profile page

--- src/app/dashboard/profile/[id]/page.tsx
+++ src/app/dashboard/profile/[id]/page.tsx
@@ -42,7 +42,7 @@ const Profile = () => {
     <div className='flex flex-1 justify-center items-center h-screen bg-slate-800 pt-16'>
         <div className='flex flex-row flex-1 h-full'>
             <div className='flex flex-col items-center w-1/4 h-full p-4 gap-4'>
-                <div className='flex flex-row items-center h-auto justify-between p-4'>
+                <div className='flex flex-row items-center w-full h-auto justify-between p-4'>
                     <h1 className='text-md text-gray-300'>
                         Full name:
                     </h1>
