5c275c30d4d2d70f01879b6f9f2254622f656f17
Arjay Onan <archessonan.1@gmail.com>
2023-10-10 10:43:54 +0800

    modified post model

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -58,7 +58,7 @@ const Post = (props: {[key: string]: any}) => {
             }
         }).then((response: AxiosResponse<any, any>) => {
             console.log(response)
-            router.refresh();
+            props.postFunction;
         }).catch((errors) => console.log(errors))
     }
 
